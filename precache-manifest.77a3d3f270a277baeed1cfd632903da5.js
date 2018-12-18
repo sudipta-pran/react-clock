984588fd884d0529c6fe32b819ae64a1a74526c2
Sudipta Bhattacharyya <jinturocks@gmail.com>
2018-12-18 19:33:38 +0530

    Updates

---
+++ precache-manifest.77a3d3f270a277baeed1cfd632903da5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-clock/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6e3cf1163772f9f8b86a",
+    "url": "/react-clock/static/js/runtime~main.6e3cf116.js"
+  },
+  {
+    "revision": "d2a1841361b52b2712c5",
+    "url": "/react-clock/static/js/main.d2a18413.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/react-clock/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "d2a1841361b52b2712c5",
+    "url": "/react-clock/static/css/main.1e816698.chunk.css"
+  },
+  {
+    "revision": "36b696c7b3a083c75021d05e0382667d",
+    "url": "/react-clock/index.html"
+  }
+];
